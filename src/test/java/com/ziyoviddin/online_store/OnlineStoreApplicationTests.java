96147a6b7d926a7b557071c0a0b4a62f5a09d39d
Ziyoviddin Mamadosimov <univerziyo@gmail.com>
2025-09-27 20:12:12 +0700

    first commit

---
+++ src/test/java/com/ziyoviddin/online_store/OnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziyoviddin.online_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
