346d55a85d0606138b045554111c568da22ca054
Ziyoviddin Mamadosimov <univerziyo@gmail.com>
2025-09-30 17:37:45 +0700

    .

---
+++ src/main/java/com/ziyoviddin/online_store/domain/product/controller/ProductsController.java
@@ -0,0 +1,16 @@
+package com.ziyoviddin.online_store.domain.product.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/products")
+public class ProductsController {
+
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ src/main/java/com/ziyoviddin/online_store/domain/product/model/Product.java
@@ -0,0 +1,13 @@
+package com.ziyoviddin.online_store.domain.product.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+
+}
